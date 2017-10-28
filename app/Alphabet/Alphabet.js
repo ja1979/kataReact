bcf1e4fc606ea3d7ba5c06c0c6c529257859556f
Jorge Aguilar <jorgeaguilar@softwareevolutivo.com.ec>
2017-10-28 18:54:34 -0500

    Lo que se pudo hacer en el Dojo en BT

--- app/Alphabet/Alphabet.js
+++ app/Alphabet/Alphabet.js
@@ -3,13 +3,30 @@ import React, {Component} from 'react';
 export class Alphabet extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {abc: ['a', 'b',
+      'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']};
   }
-  
+
+
+  rotate(array, times){
+
+    let L = array.length;
+    const n = L-times;
+    return array.slice(L - n).concat(array.slice(0, L - n));
+  }
+
+  rotateAndUpper(times) {
+    return times + ": " + this.rotate(this.state.abc, times).join("").toUpperCase();
+
+  }
+
   render() {
     return (
       <div className="container">
         Alphabet Page
+        <p>{this.rotateAndUpper(1)}</p>
+        <p>{this.rotateAndUpper(2)}</p>
+        <p>{this.rotateAndUpper(3)}</p>
       </div>
     );
   }
--- app/Alphabet/specs/Alphabet.spec.js
+++ app/Alphabet/specs/Alphabet.spec.js
@@ -5,15 +5,54 @@ import Alphabet from '../Alphabet';
 describe('Alphabet', () => {
   let component;
   let instance;
-  
+
   beforeEach(() => {
     component = shallow(<Alphabet/>);
     instance = component.instance();
   });
-  
+
   describe('on instance', () => {
     it('should defined state', () => {
-      expect(instance.state).toEqual({});
+      expect(instance.state).toEqual({abc: ['a', 'b',
+        'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']});
     });
   });
+
+  describe('on view', () => {
+    it('should find the first line', () => {
+      expect(component.find('p').first().text()).toBe('1: BCDEFGHIJKLMNOPQRSTUVWXYZA');
+    });
+    it('should find the second line', () => {
+      expect(component.find('p').at(1).text()).toBe('2: CDEFGHIJKLMNOPQRSTUVWXYZAB');
+    });
+    it('should find the third line', () => {
+      expect(component.find('p').at(2).text()).toBe('3: DEFGHIJKLMNOPQRSTUVWXYZABC');
+    });
+  });
+
+  describe('on rotate', () => {
+    it('should rotate one element 1', () => {
+      let original = ['a', 'b', 'c'];
+      let altered = ['b', 'c', 'a'];
+      expect(instance.rotate(original, 1)).toEqual(altered);
+    });
+    it('should rotate one element 2', () => {
+      let original = ['b', 'c', 'a'];
+      let altered = ['c', 'a', 'b'];
+      expect(instance.rotate(original, 1)).toEqual(altered);
+    });
+
+    it('should rotate one element an array of 5', () => {
+      let original = ['1', '2', '3', '4', '5'];
+      let altered = ['2', '3', '4', '5', '1'];
+      expect(instance.rotate(original, 1)).toEqual(altered);
+    });
+    it('should rotate two elements an array of 5', () => {
+      let original = ['1', '2', '3', '4', '5'];
+      let altered = ['3', '4', '5', '1', '2'];
+      expect(instance.rotate(original, 2)).toEqual(altered);
+    });
+
+  });
+
 });
